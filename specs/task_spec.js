58761532238e369666f70f77a54a3aec84059125
Lewis <lewisburns630@googlemail.com>
2017-06-16 18:47:29 +0100

    task has completion status

--- specs/task_spec.js
+++ specs/task_spec.js
@@ -6,11 +6,24 @@ describe('Task',function(){
   var task1;
 
   beforeEach(function(){
-   task1 = new Task("Easy");
+   task1 = new Task("Easy","whenever","A Clue",true);
  })
 
   it ("Task has difficulty level", function(){
     assert.strictEqual("Easy",task1.difficulty);
   })
+
+  it ("Task has urgency level", function(){
+    assert.strictEqual("whenever",task1.urgency);
+  })
+
+  it ("Task gives reward", function(){
+    assert.strictEqual("A Clue",task1.reward);
+  })
+
+  it ("Task has completion status", function(){
+    assert.strictEqual(true,task1.completion_status);
+  })
+
 
 })
--- task.js
+++ task.js
@@ -1,5 +1,9 @@
-var Task = function(difficulty){
+var Task = function(difficulty,urgency,reward,completion_status){
+  
   this.difficulty = difficulty;
+  this.urgency = urgency;
+  this.reward = reward;
+  this.completion_status = completion_status;
 }
 
 
