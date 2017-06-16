1748856f6fcadad0cc961122c54a657fe37a7cbd
Lewis <lewisburns630@googlemail.com>
2017-06-16 13:29:44 +0100

    hero has health and fav food

--- hero.js
+++ hero.js
@@ -1,5 +1,7 @@
-var Hero = function(name){
+var Hero = function(name,health,favourite_food){
   this.name = name;
+  this.health = health;
+  this.favourite_food = favourite_food;
 }
 
 module.exports = Hero;
--- specs/hero_spec.js
+++ specs/hero_spec.js
@@ -6,11 +6,19 @@ describe('Hero',function(){
   var hero1;
 
   beforeEach(function(){
-   hero1 = new Hero("Basil");
+   hero1 = new Hero("Basil", 100, "apples");
  })
 
   it ("Hero has name", function(){
     assert.strictEqual("Basil",hero1.name);
+  }),
+
+  it ("Hero has health", function(){
+    assert.strictEqual(100,hero1.health);
+  }),
+
+  it ("Hero has favourite food", function(){
+    assert.strictEqual("apples",hero1.favourite_food);
   })
 
 })
